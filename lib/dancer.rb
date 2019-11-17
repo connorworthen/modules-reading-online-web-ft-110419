8904d04409fad1c92023807dd7bf0e79178ab162
connor worthen <connorw220@protonmail.com>
2019-11-17 23:32:33 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,19 +1,11 @@
 require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
-module Dance
-  def twirl
-    "I'm twirling!"
-  end
+class Dancer
+  include Dance
 
-  def jump
-    "Look how high I'm jumping!"
-  end
+  attr_accessor :name
 
-  def pirouette
-    "I'm doing a pirouette"
-  end
-
-  def take_a_bow
-    "Thank you, thank you. It was a pleasure to dance for you all."
+  def initialize(name)
+    @name = name
   end
 end
